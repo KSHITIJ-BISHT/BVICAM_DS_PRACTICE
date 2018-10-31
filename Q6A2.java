b3daeedc4d3332798fcb5fbe49d7314be0157892
kshitij bisht <kshitij21bisht@gmail.com>
2018-10-31 17:12:44 +0530

    test commit

---
+++ Q6A2.java
@@ -0,0 +1,20 @@
+class Q6A2
+{
+public static void main(String[] args) {
+	int flag=0;
+
+	for(int i=0;i<5;i++)
+    { 
+    	for(int t=0;t<i;t++)
+        	System.out.print(" ");
+		for(int j=0;j<5-i-1;j++)
+			System.out.print("* ");
+        
+    System.out.println();
+    }
+ }
+}
+
+
+
+	
