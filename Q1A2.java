18edbfb33e892f73ae79dd01006cd4603667569e
kshitij bisht <kshitij21bisht@gmail.com>
2018-10-31 14:19:05 +0530

    test commit

---
+++ Q1A2.java
@@ -0,0 +1,36 @@
+class Q1A2
+{
+
+	public static void main(String[] args) {
+		int space=2;int star=2,i;
+		for( i=0;i<=5/2;i++)
+		{
+			
+			for(int j=0;j<space;j++)
+			{
+              System.out.print(" ");
+              
+			}
+			space--;
+			for(int k=0;k<i+1;k++)
+				System.out.print("* ");
+			System.out.println();
+        }
+
+			space=1;
+			for(int t=i+1;t<=5;t++){
+		    	for(int j=0;j<space;j++)
+			    {
+              System.out.print(" ");
+             
+			    }
+			space++;
+			for(int k=0;k<=5-t;k++)
+				System.out.print("* ");
+           	System.out.println();
+			}
+		}
+		
+		
+		}
+	
