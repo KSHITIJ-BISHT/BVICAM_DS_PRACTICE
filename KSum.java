b48f9b60485ec6de3f44d51ee455c4c7c31e151b
kshitij bisht <kshitij21bisht@gmail.com>
2018-11-03 20:27:40 +0530

    pairs forming the sum

---
+++ KSum.java
@@ -0,0 +1,24 @@
+//find the pairs for the given sum
+import java.util.*;
+class KSum{
+	public static void main(String[] args) {
+		int[] arr=new int[]{10,4,5,1,8,3,6};
+		int sum=7;
+		HashSet<Integer> map=new HashSet<>();
+
+		for (int i=0;i<arr.length ;i++ ) {
+			int diff=sum-arr[i];
+			if(map.contains(diff))
+			{
+				System.out.println(diff+" "+arr[i]);
+
+
+			}
+			else
+			{	map.add(arr[i]);
+
+			}
+		}
+		//System.out.println(map);
+	}
+}
