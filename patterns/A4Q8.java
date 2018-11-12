f9761360341f254ed8cee1f8d1f2e1b04fbd8443
KSHITIJ-BISHT <42808607+KSHITIJ-BISHT@users.noreply.github.com>
2018-11-12 23:08:49 +0530

    pattern

---
+++ patterns/A4Q8.java
@@ -0,0 +1,27 @@
+/*
+333222111
+332211
+321
+
+*/
+class A4Q8
+{
+	public static void main(String[] args) {
+		int n=3;
+		int mul=0;
+		for(int k=0;k<n;k++)
+		{
+			mul=mul*10+1;
+		}
+		for (int i=0;i<n ;i++ ) {
+			for(int j=n;j>0;j--)
+			{
+				System.out.print(j*mul);
+
+			}
+			mul=mul/10;
+		    System.out.println();	
+		}
+
+	}
+}
