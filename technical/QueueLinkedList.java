aab4aa993a00a8f88d2a88895db90ca80546a0ca
KSHITIJ-BISHT <42808607+KSHITIJ-BISHT@users.noreply.github.com>
2018-11-15 23:32:05 +0530

    circular queue with linked list

---
+++ technical/QueueLinkedList.java
@@ -0,0 +1,93 @@
+
+  
+// circular queue using linked list
+
+class QueueLinkedList 
+{ 
+    
+// Structure of a Node  
+static class Node  
+{  
+    int data;  
+     Node  link;  
+} 
+    
+static class Queue  
+{  
+     Node  front,  rear;  
+} 
+    
+// Function to create Circular queue  
+static void enQueue(Queue  q, int value)  
+{  
+     Node  temp = new Node();  
+    temp .data = value;  
+    if (q .front ==  null)  
+        q .front = temp;  
+    else
+        q .rear .link = temp;  
+    
+    q .rear = temp;  
+    q .rear .link = q .front;  
+}  
+    
+// Function to delete element from Circular Queue  
+static  int deQueue(Queue  q)  
+{  
+    if (q .front ==  null)  
+    {  
+        System.out.print ("Queue is empty");  
+        return Integer.MIN_VALUE;  
+    }  
+  
+    int value; 
+    if (q .front == q .rear)  
+    {  
+        value = q .front .data;  
+        q .front =  null;  
+        q .rear =  null;  
+    }  
+    else  
+    {  
+         Node  temp = q .front;  
+        value = temp .data;  
+        q .front = q .front .link;  
+        q .rear .link= q .front;  
+    }  
+    
+    return value ;  
+}  
+ 
+static void displayQueue( Queue  q)  
+{  
+     Node  temp = q .front;  
+    System.out.println("\nElements in Circular Queue are: ");  
+    while (temp .link != q .front)  
+    {  
+        System.out.println(" "+ temp .data);  
+        temp = temp .link;  
+    }  
+    System.out.println(" "+ temp .data);  
+}  
+ 
+public static void main(String args[]) 
+{  
+     
+    Queue  q = new Queue();  
+    q .front = q .rear =  null;  
+     System.out.print("\nDeleted value = "+ deQueue(q));  
+    enQueue(q, 14);  
+    enQueue(q, 22);  
+    enQueue(q, 6);  
+    
+    displayQueue(q);    
+    System.out.print("\nDeleted value = "+ deQueue(q));  
+    System.out.println("\nDeleted value ="+ deQueue(q));    
+    displayQueue(q);  
+    
+    enQueue(q, 9);  
+    enQueue(q, 20);  
+    displayQueue(q);  
+    
+}  
+} 
